ade155eb3b1a8d2fb3abc1cbd49714d7a627f8c2
zhaoxinnZ <zhaoxinzhang0429@gmail.com>
2022-04-29 11:19:01 +0800

    fixed

---
+++ src/com/fju/ai/Box2.java
@@ -0,0 +1,13 @@
+package com.fju.ai;
+
+public class Box2 extends Box{
+
+    public Box2() {
+        super(20, 20, 20);
+    }
+
+    @Override
+    public String toString(){
+        return "二號箱";
+    }
+}
---
+++ src/com/fju/ai/Box3.java
@@ -0,0 +1,12 @@
+package com.fju.ai;
+
+public class Box3 extends Box{
+    public Box3() {
+        super(30, 30, 30);
+    }
+
+    @Override
+    public String toString(){
+        return "三號箱";
+    }
+}
